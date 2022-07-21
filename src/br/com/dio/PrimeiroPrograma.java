e19a942de900bab651300e5960b66dd92f236f8e
juliet <juliete@link3.com.br>
2022-07-20 22:51:49 -0300

    up class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,8 +6,10 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
 
         Gato gato = new Gato();
-
              System.out.println(gato);
+
+        livro livro1 = new livro("Seja feliz para sempre", 257);
+        System.out.println(livro1);
 
 //        int a = 5;
 //        int b = 3;
@@ -15,6 +17,41 @@ public class PrimeiroPrograma {
 
     }
 
+}
+
+class livro{
+
+    private String nome;
+    private Integer numPaginas;
+
+    public livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
 
 
